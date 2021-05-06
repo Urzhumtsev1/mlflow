c3b21f629a372b7f9dc3bba2ea7d28061776424c
Maxim Urzhumtsev <urzhumtsev.max@gmail.com>
2021-05-06 20:10:31 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+from dotenv import dotenv_values
 from tensorflow import keras
+
+import mlflow
+import mlflow.keras
+import mlflow.tensorflow
+
+config = dotenv_values(".env")
+PG_USER=config['POSTGRES_USER']
+PG_PASS=config['POSTGRES_PASSWORD']
+PG_HOST=config['PG_HOST']
+PG_PORT=config['PG_PORT']
+
+uri=f"postgresql+psycopg2://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/mlflow"
+mlflow.set_tracking_uri(uri)
+mlflow.set_experiment('exp_1')
+mlflow.tensorflow.autolog()
+mlflow.keras.autolog()
+
+mlflow.start_run()
 
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
@@ -11,4 +30,6 @@ ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
 model.fit(xs, ys, epochs=500)
 
-print(model.predict([10.0]))
+# print(model.predict([10.0]))
+
+mlflow.end_run()
