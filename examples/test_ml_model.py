f06f602082bc8ea9474032cdbf353b33f1f64479
Maxim Urzhumtsev <urzhumtsev.max@gmail.com>
2021-05-17 13:53:32 +0300

    model name

--- examples/test_ml_model.py
+++ examples/test_ml_model.py
@@ -1,5 +1,8 @@
 import mlflow
-logged_model = 's3://mlflow/artifacts/exp_1/6863a1daf71f4f608569cb038a500f6d/artifacts/model'
+
+model_name = 'model_test'
+stage = 'Staging'
+logged_model = f'models:/{model_name}/{stage}'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
